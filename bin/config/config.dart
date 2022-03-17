142efb93b3d4462f2a80481972a163de6f05e62a
eren <erndemir.1@gmail.com>
2022-03-17 16:19:10 +0300

    commit

--- bin/config/config.dart
+++ bin/config/config.dart
@@ -1,12 +1,9 @@
 import '../utils/app_utils.dart';
-import 'app_config.dart';
 import 'auth_config.dart';
 import 'db_config.dart';
 
 abstract class Config {
-  final appConfig = AppConfig();
   final appUtils = AppUtils();
   final dbConfig = DbConfig();
   final jwtAuth = AuthConfig();
-
 }
